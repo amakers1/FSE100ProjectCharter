fe6d6ff2a258489b9b7842f21d63415d2a5215c4
Jaden Figger <jadenfigger@gmail.com>
2022-10-24 13:53:38 -0700

    added class functionality as well as functions to add and draw buttons

--- sketch.js
+++ sketch.js
@@ -2,19 +2,41 @@ gameState = "home";
 
 function setup() {
   createCanvas(400, 400);
+
+  home = new Home();
+  home.addButton("button", [255, 0, 0], 200, 200, 50, 30);
 }
 
 function draw() {
   background(220);
+
+  home.draw();
 }
 
 function mouseClicked() {}
 
+
 // drawing the home screen
-function drawHome() {}
+class Home {
+	constructor() {
+		this.buttons = []
+	}
+
+	addButton(text, color, x, y, width, height) {	
+		this.buttons.push({"text": text, "color": color, "x": x, "y": y, "width": width, "height": height});
+	}
+
+	draw() {
+		for (let i = 0; i < this.buttons.length; i++) {
+			fill(this.buttons[i].color[0], this.buttons[i].color[1], this.buttons[i].color[2]);
+			rect(this.buttons[i].x, this.buttons[i].y, this.buttons[i].width, this.buttons[i].height);
+		}
+	}
+}
 
 // drawing the holding a pencil exercise screen
 function drawHAP() {}
 
 // drawing the throwing and catching exercise screen
 function drawTC() {}
+
