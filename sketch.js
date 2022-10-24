f3dd7a31f11af9588640ce5c45173066534c6288
Jaden Figger <jadenfigger@gmail.com>
2022-10-24 13:41:03 -0700

    First commit Initialized the boiler plate code

---
+++ sketch.js
@@ -0,0 +1,20 @@
+gameState = "home";
+
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+function mouseClicked() {}
+
+// drawing the home screen
+function drawHome() {}
+
+// drawing the holding a pencil exercise screen
+function drawHAP() {}
+
+// drawing the throwing and catching exercise screen
+function drawTC() {}
