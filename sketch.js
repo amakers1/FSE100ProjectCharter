546cbd54f11452d7872e6708a1d65bd1fb7f161f
Jaden Figger <jadenfigger@gmail.com>
2022-10-24 14:06:47 -0700

    setting up code to manage different game states

--- sketch.js
+++ sketch.js
@@ -1,16 +1,31 @@
+// home - main menu
+// hap - holding pencil exercise
+// tc - throwing and catching exercise
 gameState = "home";
 
-function setup() {
-  createCanvas(1280, 720);
+function preload() {
+	img = loadImage('assets/laDefense.jpg');
+  }
 
-  home = new Home();
-  home.addButton("button", [255, 0, 0], 200, 200, 50, 30);
+function setup() {
+	createCanvas(1280, 720);
+
+	home = new Home();
+	home.addButton("button", [255, 0, 0], 200, 200, 50, 30);
 }
 
 function draw() {
-  background(220);
+  	background(220);
 
-  home.draw();
+	if (gameState == "home") {
+		home.draw();
+	} 
+	else if (gameState == "hap") {
+		home.draw();
+	} 
+	else if (gameState == "tc") {
+		home.draw();
+	} 
 }
 
 function mouseClicked() {}
