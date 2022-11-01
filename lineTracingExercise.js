227d85dd0c29e055e11e6f2106b2ea69711766ef
Jaden Figger <jadenfigger@gmail.com>
2022-11-01 12:24:07 -0700

    continuing with making line tracing exercise

--- lineTracingExercise.js
+++ lineTracingExercise.js
@@ -1,53 +1,119 @@
-mouseEnteredStart = false;
-lineStrokeWeight = 15;
+let mouseEnteredStart = false;
+let lineStrokeWeight = 15;
+let lineCords = [];
+let currIndexOfCenter = 0;
+
+let lineTracingScore = 0;
+let lineTracingTime = 0.0;
+let pauseLTExercise = false;
+
 
 // line tracing exercise screen
 function lineTracingExercise() {
 	drawGUI();
 
-	if (exerciseStart) {
-		drawLine(550, 200, 750, 300, primaryColor, secondaryColor);
-		drawLine(750, 300, 575, 600, primaryColor, secondaryColor);
-		drawLine(575, 600, 775, 450, primaryColor, secondaryColor);
-		drawLine(775, 450, 815, 150, primaryColor, secondaryColor);
-		drawLine(815, 150, 950, 610, primaryColor, secondaryColor);
-		drawLine(950, 610, 980, 310, primaryColor, secondaryColor);
-		drawLine(980, 310, 1150, 200, primaryColor, secondaryColor);
-		drawLine(1150, 200, 1180, 610, primaryColor, secondaryColor);
+	drawLine(550, 200, 750, 300, 0);
+	drawLine(750, 300, 575, 600, 1);
+	drawLine(575, 600, 775, 450, 2);
+	drawLine(775, 450, 815, 150, 3);
+	drawLine(815, 150, 950, 610, 4);
+	drawLine(950, 610, 980, 310, 5);
+	drawLine(980, 310, 1150, 200, 6);
+	drawLine(1150, 200, 1180, 610, 7);
 
-		// start circle
-		fill(12, 166, 20);
-		stroke(15, 92, 5);
-		ellipse(550, 200, 50, 50);
 
-		// end circle
-		fill(166, 22, 12);
-		stroke(92, 10, 5);
-		ellipse(1180, 610, 50, 50);
+	// start circle
+	fill(12, 166, 20);
+	stroke(15, 92, 5);
+	ellipse(550, 200, 50, 50);
 
-		// if mouse entered start area
-		if (dist(mouseX, mouseY, 550, 200) <= 25) {
-			mouseEnteredStart = true;
-		}
+	// end circle
+	fill(166, 22, 12);
+	stroke(92, 10, 5);
+	ellipse(1180, 610, 50, 50);
 
-		if (mouseEnteredStart) {
-			c = get(mouseX, mouseY);
-			mouseColor = color(c[0], c[1], c[2]);
+	// draw "Start Here" text
+	drawStartText(currIndexOfCenter);
 
-			if (arrayEquals(primaryColor || mouseColor == secondaryColor) {
-				console.log("inside")
-			} 
-			else {
-				console.log("outside")
-			}
+	// console.log("pause: " + pauseLTExercise + " | start: " + exerciseStart)
+
+	// if the mouse entered the starting area, start the game
+	if (dist(mouseX, mouseY, lineCords[currIndexOfCenter][0], lineCords[currIndexOfCenter][1]) <= 30) {
+		exerciseStart = true;
+		pauseLTExercise = false;
+	}
+	// if the game has started and the mouse has left the starting area
+	if (exerciseStart && !pauseLTExercise && dist(mouseX, mouseY, lineCords[currIndexOfCenter][0], lineCords[currIndexOfCenter][1]) > 30) {
+		c = get(mouseX, mouseY);
+		// if the current color at the mouse position is apart of the line (if the player is still in the line)
+		if (arrayEqualsArrays(c, lineTracingColors)) {
+			console.log("inside");
+
+			if (dist(mouseX, mouseY, lineCords[currIndexOfCenter][2], lineCords[currIndexOfCenter][3]) < 30) {
+				lineCords[currIndexOfCenter][4] = true;
+				currIndexOfCenter++;
+				console.log(lineCords)
+			}		
+		} 
+		else {
+			console.log("outside");
+			lineCords[currIndexOfCenter][4] = false;
+			currIndexOfCenter++;
+			pauseLTExercise = true;
 		}
 	}
 }
 
+function drawStartText(index) {
+	stroke(primaryColor, 230);
+	strokeWeight(3);
+	fill(255, 255, 255);
+	textSize(24);
+	text("Start Here", lineCords[index][0], lineCords[index][1] - 45);
+}
 
-function drawLine(x0, y0, x1, y1, inner, outer) {
-	fill(inner);
-	stroke(outer);
+
+function closestDistToCenter() {
+	let shortestDist = 10000;
+	let mX = mouseX;
+	let mY = mouseY;
+	for (let i = 0; i < lineCords.length; i++) {
+		let d = dist(lineCords[i][2], lineCords[i][3], mX, mY);
+		if (d < shortestDist) {
+			shortestDist = d;
+		}
+	}
+	return shortestDist;
+}
+
+
+
+
+function drawLine(x0, y0, x1, y1, index) {
+	// if lineCords has been created
+	if(typeof lineCords[index] !== 'undefined') {
+		// if the line segment should be drawn green
+		if (lineCords[index][4]) {
+			fill([11, 212, 21]);
+			stroke([9, 179, 17]);
+		}
+		// it should be drawn with the default colors
+		else if (lineCords[index][4] == false) {
+			fill(224, 21, 18);
+			stroke(171, 14, 12);
+		}
+		else if (lineCords[index][4] == undefined){
+			fill(primaryColor);
+			stroke(secondaryColor);
+		}
+	}
+	// if lineCords has not been created yet.
+	else {
+		lineCords.push([x0, y0, x1, y1, undefined]);
+		fill(primaryColor);
+		stroke(secondaryColor);
+	}	
+
 	strokeWeight(lineStrokeWeight);
 	line(x0, y0, x1, y1);
 	strokeWeight(3);
@@ -69,7 +135,7 @@ function drawGUI() {
 	strokeWeight(3);
 	fill(10, 10, 10, 150);
 	rect(width/6 + 10, height/8 + 10, width/3 - 20, 100, 50, 0, 50, 0);
-	fill(255, 255, 255)
+	fill(255, 255, 255);
 	textAlign(CENTER, CENTER);
 	textStyle(BOLDITALIC);
 	textSize(56);
@@ -83,6 +149,7 @@ function drawGUI() {
 	strokeWeight(2);
 	rect(20, 200, width/3-20, 400);
 
+	// drawing how to play text
 	textStyle(NORMAL);
 	textSize(32);
 	noStroke();
@@ -93,18 +160,11 @@ function drawGUI() {
 	stroke(primaryColor);
 	line(40, 260, width/3-20, 260);
 
-	// draw start button
-	let sButton = createButton("START")
-	sButton.position(305, 650)
-	sButton.size(120, 50)
-	sButton.style("background-color", buttonColor)
-	sButton.style("color", textColor)
-	sButton.style("border-width: 3px")
-	sButton.style("border: solid")
-	sButton.style("border-color", color(15, 131, 176))
-	sButton.style("font-size", 24)
-	sButton.mousePressed(() => { startLineTracingGame() });
-	activeButtons.push(sButton);
+	// draw score and time text
+	textSize(50);
+	text("How to play", width/6, 240);
+	text(`Score: ${lineTracingScore}`, 500, 100);
+	text(`Time: ${lineTracingTime}`, 1000, 100);
 }
 
 function startLineTracingGame() {
--- sketch.js
+++ sketch.js
@@ -17,6 +17,8 @@ let standardButtonWidth;
 let standardButtonHeight;
 let buttonTextSize;
 
+let lineTracingColors;
+
 function preload() {
 	backgroundImg = loadImage('assets/1280x720.png');
   }
@@ -26,11 +28,15 @@ function setup() {
 
 	standardButtonWidth = width/3;
 	standardButtonHeight = height/10;
-	secondaryColor = color(20, 167, 224);
-	primaryColor = color(15, 131, 176);
+	// secondaryColor = color(20, 167, 224);
+	secondaryColor = [21, 167, 224];
+	// primaryColor = color(15, 131, 176);
+	primaryColor = [16, 131, 176];
 	buttonColor = color(43, 43, 43);
 	textColor = color(220, 220, 220);
 	buttonTextSize = "28px";
+
+	lineTracingColors = [[secondaryColor[0], secondaryColor[1], secondaryColor[2], 255], [primaryColor[0], primaryColor[1], primaryColor[2], 255], [12, 166, 20, 255], [15, 92, 5, 255]];
 }
 
 function draw() {
@@ -73,6 +79,23 @@ function drawBackButton(xoffset=0, yoffset=0) {
 	activeButtons.push(bButton);
 }
 
-function drawStartButton(xoffset=0, yoffset=0) {
-	
+function arraysEqual(array1, array2) {
+	if (array1.length != array2.length) {
+		return false;
+	}
+	for (let i = 0; i < array1.length; i++) {
+		if (array1[i] != array2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+function arrayEqualsArrays(array1, largerArray) {
+	for (let i = 0; i < largerArray.length; i++) {
+		if (arraysEqual(array1, largerArray[i])) {
+			return true;
+		}
+	}
+	return false;
 }
