3362ed836c5e7382de2675699d197b9aad90f51d
Jaden Figger <jadenfigger@gmail.com>
2022-11-02 14:22:08 -0700

    editing gui for pinpoint exercise

--- pinpointExercise.js
+++ pinpointExercise.js
@@ -1,3 +1,4 @@
+
 function pinpointExercise() {
 	drawGUIPinpoint();
 }
@@ -5,23 +6,25 @@ function pinpointExercise() {
 function drawGUIPinpoint() {
 	drawBackButton(10);
 
+	xoffset = 2*width/3 - 20;
+
 	// background rectangle
 	rectMode(CORNER)
 	fill(10, 10, 10, 50);
 	stroke(15, 131, 176, 230);
-	rect(10, 10, width/3, height-20);
+	rect(xoffset + 10, 10, width/3, height-20);
 
 	// title
 	rectMode(CENTER)
 	stroke(primaryColor, 230);
 	strokeWeight(3);
 	fill(10, 10, 10, 150);
-	rect(width/6 + 10, height/8 + 10, width/3 - 20, 100, 50, 0, 50, 0);
+	rect(xoffset + width/6 + 10, height/8 + 10, width/3 - 20, 100, 50, 0, 50, 0);
 	fill(255, 255, 255)
 	textAlign(CENTER, CENTER);
 	textStyle(BOLDITALIC);
 	textSize(56);
-	text("Line Tracing", width/6+10, height/8 + 10);
+	text("Line Tracing", xoffset + width/6+10, height/8 + 10);
 
 
 	// background rectangle
@@ -29,17 +32,17 @@ function drawGUIPinpoint() {
 	fill(10, 10, 10, 50);
 	stroke(primaryColor, 200);
 	strokeWeight(2);
-	rect(20, 200, width/3-20, 400);
+	rect(xoffset + 20, 200, width/3-20, 400);
 
 	textStyle(NORMAL);
 	textSize(32);
 	noStroke();
 	fill(primaryColor)
-	text("How to play", width/6, 230);
+	text("How to play", xoffset + width/6, 230);
 
 	// line under How to Play
 	stroke(primaryColor);
-	line(40, 260, width/3-20, 260);
+	line(xoffset + 40, 260, width/3-20, 260);
 
 	// draw reset button
 	drawResetButton();
